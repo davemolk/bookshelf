4b206f27a4cbebeaf83e05b3c1ffcc3b9bda5fc1
davemolk <davemolk@gmail.com>
2021-10-21 10:56:09 -0600

    custom user model, basic

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+
+    # local
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
