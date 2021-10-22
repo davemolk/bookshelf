97d22f83f96ad1d59a397ca757bd02d154bb9200
davemolk <davemolk@gmail.com>
2021-10-22 10:19:26 -0600

    startapp books and register

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('books/', include('books.urls')),
     path('', include('pages.urls')),
-
 ]
