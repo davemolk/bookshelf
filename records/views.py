7973fc66094d24762592a4d995cfe6c880678970
davemolk <davemolk@gmail.com>
2021-10-24 05:06:07 -0600

    basic crud records

--- records/views.py
+++ records/views.py
@@ -6,7 +6,51 @@ from django.shortcuts import (
     get_object_or_404
 )
 
+from .forms import RecordForm
 from .models import Record
 
 
+@login_required
+def record_list_view(request):
+    qs = Record.objects.filter(owner=request.user)
+    context = {
+        'object_list': qs,
+    }
+    return render(request, 'records/list.html', context)
 
+
+@login_required
+def record_detail_view(request, slug):
+    obj = get_object_or_404(Record, slug=slug, owner=request.user)
+    context = {
+        'object': obj,
+    }
+    return render(request, 'records/detail.html', context)
+
+
+@login_required
+def record_create_view(request):
+    form = RecordForm(request.POST or None)
+    context = {
+        'form': form,
+    }
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.owner = request.user
+        obj.save()
+        return redirect(obj.get_absolute_url())
+    return render(request, 'records/create_update.html', context)
+
+
+@login_required
+def record_update_view(request, slug):
+    obj = get_object_or_404(Record, slug=slug, owner=request.user) 
+    form = RecordForm(request.POST or None, instance=obj)
+    context = {
+        'form': form,
+        'object': obj,
+    }
+    if form.is_valid():
+        form.save()
+        context['message'] = 'Save successful!'
+    return render(request, 'records/create_update.html', context)
