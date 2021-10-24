54c71048ff9b96f1d88288f0b30919acb7129abd
davemolk <davemolk@gmail.com>
2021-10-24 05:29:39 -0600

    record urls, admin (basic)

--- records/urls.py
+++ records/urls.py
@@ -1,2 +1,17 @@
 from django.urls import path
 
+from .views import(
+    record_list_view,
+    record_detail_view,
+    record_create_view,
+    record_update_view,
+)
+
+app_name='records'
+
+urlpatterns = [
+    path('', record_list_view, name='list'),
+    path('create/', record_create_view, name='create'),
+    path('<slug:slug>/update/', record_update_view, name='update'),
+    path('<slug:slug>/', record_detail_view, name='detail'),
+]
