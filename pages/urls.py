c1d8e91546ad0ec7013b86470b2df42feec99802
davemolk <davemolk@gmail.com>
2021-10-26 12:31:25 -0600

    refactor searches

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import (
+    HomePageView, 
+    AboutPageView,
+    SiteSearchResults,
+)
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('search/', SiteSearchResults.as_view(), name='all_search_results'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,39 @@
-from django.views.generic import TemplateView
+from django.views.generic import (
+    TemplateView,
+    ListView,
+)
+from django.db.models import Q
+from itertools import chain
+from books.models import Book
+from records.models import Record
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
+
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+
+class SiteSearchResults(ListView):
+    model = Book
+    template_name = 'search_results.html'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(SiteSearchResults, self).get_context_data(**kwargs)
+    #     context['record_list'] = Record.objects.all()
+    #     return context
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        book_results = Book.objects.filter(owner=self.request.user).filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+            | Q(notes__icontains=query)
+        ).order_by('title')
+        record_results = Record.objects.filter(owner=self.request.user).filter(
+             Q(title__icontains=query) | Q(musicians__icontains=query)
+            | Q(notes__icontains=query)
+        ).order_by('title')
+        qs_chain = chain(book_results, record_results)
+        return qs_chain
